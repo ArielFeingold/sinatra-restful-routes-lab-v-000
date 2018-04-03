6c9c50c30ea8f96d6828a2c8bab0cf80dc1be57d
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-03 19:22:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,7 @@ class ApplicationController < Sinatra::Base
   delete '/recipes/:id/delete' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
+    redirect '/recipes'
   end
 
   get '/recipes/:id/edit' do  #load edit form
