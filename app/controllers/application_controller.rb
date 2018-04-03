50e8908473e5672afd7820f62b9dbe5a273ec780
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-03 18:29:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,17 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-    erb :index
-  end
-
   get '/recipes' do
-    @recipes = Recipe.all
-
     erb  :index
   end
 
   get '/recipes/new' do
     erb :new
+  end
+
+  post '/recipes' do
+    @recipes = Recipe.all
+    erb :index
   end
 
 end
