6d85eda47bc0e304155a9b0de0db415a98b377e4
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-03 18:41:18 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,5 @@ class ApplicationController < Sinatra::Base
     erb  :index
   end
 
-  get '/recipes/new' do
-    erb :new
-  end
-
-  post '/recipes/new' do
-    binding.pry
-    # Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time[:cook_time])
-  end
 
 end
